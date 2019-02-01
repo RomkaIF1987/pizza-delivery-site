09244c3697164b60e7d1f890542f983d9a92e1af
Roman Zhyliak <romka.if87@gmail.com>
2019-02-01 19:48:20 +0200

    Add auth make users table migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,13 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 10)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('city');
+            $table->string('street');
+            $table->string('house');
+            $table->string('room');
             $table->rememberToken();
             $table->timestamps();
         });
